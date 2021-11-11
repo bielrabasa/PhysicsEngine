ea054174a9fa451cb2028516457d8b2503d337ee
Biel Rabasa <bielrabasa@gmail.com>
2021-11-11 14:47:18 +0100

    Imprimir

--- PhysicsEngineTest1/ModulePhysics.cpp
+++ PhysicsEngineTest1/ModulePhysics.cpp
@@ -17,6 +17,7 @@ bool ModulePhysics::Start()
 	LOG("Creating Physics 2D environment");
 
 	balls.add(CreateBall(100, 100, 50));
+	balls.add(CreateBall(200, 50, 10));
 
 	return true;
 }
@@ -24,15 +25,13 @@ bool ModulePhysics::Start()
 
 update_status ModulePhysics::PreUpdate()
 {
-	App->renderer->DrawCircle(balls.getLast()->data->x, balls.getLast()->data->y, balls.getLast()->data->radius, 250, 250, 250);
-	App->renderer->DrawCircle(200, 200, 10, 250, 250, 250);
-
 	return UPDATE_CONTINUE;
 }
 
 
 update_status ModulePhysics::PostUpdate()
 {
+	App->renderer->DrawCircle(balls.getLast()->data->x, balls.getLast()->data->y, balls.getLast()->data->radius, 250, 250, 250);
 
 	if(App->input->GetKey(SDL_SCANCODE_F1) == KEY_DOWN)
 		debug = !debug;
